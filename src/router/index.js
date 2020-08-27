af24afb238c30a75287b5fb24de1951a89524764
1135834408@qq.com <1135834408@qq.com>
2020-08-27 16:07:48 +0800

    新上传

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Msite from '../pages/Msite/Msite'
+import Search from '../pages/Search/Search'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  route: [
+    {
+      path: '/msite',
+      component: Msite
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    }
+  ]
+})
+
+export default router
